a7d9e8053e39bf3799b9250c5df0c019160680aa
gibson xue <gibsonxue@hotmail.com>
2025-11-10 21:17:52 +0800

    add celery service file

--- graphs/common/TaskRunner.py
+++ graphs/common/TaskRunner.py
@@ -10,7 +10,7 @@ class TaskRunner:
     Class-based handler for generating and storing exam outlines and papers.
     """
 
-    def __init__(self, level: str, exam_type: ExamType, task_id: str):
+    def __init__(self, level: str, exam_type: ExamType, task_id: Optional[str] = None):
         self.task_id = task_id
         self.level = level.lower()
         self.exam_type = exam_type
